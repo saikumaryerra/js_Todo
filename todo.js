7e6d4d88ea740fdaf9bff2c4b14b515f64a7832d
saikumar <yerra.sai@mountblue.io>
2019-06-05 09:39:54 +0530

    changed p to input and placed checkbox && space wont be considered as input anymore

--- todo.js
+++ todo.js
@@ -1,20 +1,34 @@
-function addingToList(x){
-	var mainSection=document.createElement('div');
-	var listHead=document.createElement('p');
-	var listContent=document.createTextNode(x);
-	listHead.appendChild(listContent);
-	mainSection.appendChild(listHead);	
-	var todoList = document.getElementById('sai');
-	todoList.appendChild(mainSection);
-	document.getElementById('checklist').value=""
+function addingToList(x) {
+    var mainSection = document.createElement('div');
+    // var listHead=document.createElement('p');
+    // var listContent=document.createTextNode(x);
+    // listHead.appendChild(listContent);
+    // mainSection.appendChild(listHead);	
+    // var todoList = document.getElementById('sai');
+    // todoList.appendChild(mainSection);
+    // document.getElementById('checklist').value="";
 
-	// Not workings
-	// todoList.insertAdjacentHTML('afterbegin',mainSection);
+    var listHead = document.createElement('input');
+    listHead.setAttribute('type', 'checkbox');
+
+    var label = document.createElement('label');
+    var labelContent=document.createTextNode(x);
+    label.appendChild(labelContent);
+    mainSection.appendChild(listHead);
+    mainSection.appendChild(label);    
+    var todoList = document.getElementById('sai');
+    todoList.appendChild(mainSection);
+    document.getElementById('checklist').value = "";
+    // Not workings
+    // todoList.insertAdjacentHTML('afterbegin',mainSection);
 }
 
-document.getElementById('checklist').addEventListener('keypress',function(event){
-	if (event.keyCode == 13) {
-		var inputData = document.getElementById('checklist').value;
-		addingToList(inputData);
-	}
+document.getElementById('checklist').addEventListener('keypress', function(event) {
+    var temp = document.getElementById('checklist').value;
+    var inputData=String(temp);
+    inputData=inputData.trim();
+
+    if (event.keyCode == 13 && inputData !== "") {        
+        addingToList(inputData);
+    }
 });
