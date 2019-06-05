49101a5f5527726acafefb3ce03eed260cc0ca99
saikumar <yerra.sai@mountblue.io>
2019-06-05 19:32:23 +0530

    added functions to all footer elements,hide/unhide of footer also done

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
-var totalElements=0;
-var checkedElements=0;
-var activeElements=0;
+var totalElements = 0;
+var checkedElements = 0;
+var activeElements = 0;
 // Adding Element
 document.getElementById('checklist').addEventListener('keypress', function(event) {
     var temp = document.getElementById('checklist').value;
@@ -9,7 +9,16 @@ document.getElementById('checklist').addEventListener('keypress', function(event
     if (event.keyCode == 13 && inputData !== "") {
         addingToList(inputData);
     }
+
 });
+//initially hiding footer i.e when no elements
+// document.getElementById('bottom-bar').style.display="none";
+document.getElementById('bottom-bar').setAttribute('class','hide');
+// document.getElementsByTagName('span').setAttribute('class','hide');
+
+// ab.setAttribute('class','hide');
+// ab.classList.add('hide');
+
 // adding element Function
 function addingToList(x) {
     totalElements++;
@@ -17,8 +26,8 @@ function addingToList(x) {
 
     var listHead = document.createElement('input');
     listHead.setAttribute('type', 'checkbox');
-    // listHead.setAttribute('class', 'uncheck');
-    listHead.setAttribute('onclick','check()')
+    listHead.setAttribute('class', 'uncheck');
+    listHead.setAttribute('onclick', 'check()')
 
     var label = document.createElement('label');
     var labelContent = document.createTextNode(x);
@@ -26,7 +35,7 @@ function addingToList(x) {
 
     var crossMark = document.createElement('span');
     // crossMark.setAttribute('onclick', 'this.parentElement.style.display=\'none\';');
-    crossMark.setAttribute('onclick','removeList()')
+    crossMark.setAttribute('onclick', 'removeList()')
     crossMark.innerHTML = "&#10005;";
 
 
@@ -38,31 +47,100 @@ function addingToList(x) {
     todoList.appendChild(mainSection);
     document.getElementById('checklist').value = "";
     noOfActive();
+    footerDisplay();
     // Not workings
     // todoList.insertAdjacentHTML('afterbegin',mainSection);
 }
 //removing element function
-function removeList(){ 
-    event.target.parentElement.remove();  
+function removeList() {
+    event.target.parentElement.remove();
     totalElements--;
     noOfActive();
+    footerDisplay();
 }
 //to display no'of active elements
-function noOfActive(){
-    activeElements=totalElements-checkedElements;
-    document.getElementById('items-count').innerHTML=activeElements+" items left";
+function noOfActive() {
+    activeElements = totalElements - checkedElements;
+    document.getElementById('items-count').innerHTML = activeElements + " items left";
 }
 //to checkbox
-function check(){
-    if(event.target.checked){
+function check() {
+    if (event.target.checked) {
         checkedElements++;
-           event.target.classList.toggle("check");
-           event.target.parentElement.querySelector('label').classList.toggle("strike");
-    }
-    else {
+        event.target.classList.toggle("check");
+        event.target.classList.toggle("uncheck");
+        event.target.parentElement.querySelector('label').classList.toggle("strike");
+    } else {
         checkedElements--;
         event.target.classList.toggle("check");
+        event.target.classList.toggle("uncheck");
         event.target.parentElement.querySelector('label').classList.toggle("strike");
     }
     noOfActive();
+}
+//to displayall
+function displayAll() {
+    var a = document.querySelector('#todo-list').querySelectorAll('.hide');
+    for (var i = a.length - 1; i >= 0; i--) {
+        // a[i].parentElement.setAttribute('class','hide');
+        a[i].classList.toggle('hide');
+    }
+}
+// to display active elements
+function displayActive() {
+    var a = document.querySelectorAll('.check');
+    for (var i = a.length - 1; i >= 0; i--) {
+        a[i].parentElement.setAttribute('class', 'hide');
+    }
+    var a = document.querySelectorAll('.uncheck');
+    for (var i = a.length - 1; i >= 0; i--) {
+        a[i].parentElement.classList.remove('hide');
+    }
+
+}
+// to display completed
+function displayCompleted() {
+    var a = document.querySelectorAll('.uncheck');
+    for (var i = a.length - 1; i >= 0; i--) {
+        a[i].parentElement.setAttribute('class', 'hide');
+    }
+    var a = document.querySelectorAll('.check');
+    for (var i = a.length - 1; i >= 0; i--) {
+        a[i].parentElement.classList.remove('hide');
+    }
+}
+//to clear elements
+function clearItems() {
+    document.querySelector('#todo-list').innerHTML = "";
+
+    totalElements = 0;
+    checkedElements = 0;
+    activeElements = 0;
+    noOfActive();
+    footerDisplay();
+}
+
+//to clear completed
+function clearCompleted() {
+    var a = document.querySelectorAll('.check');
+    totalElements-=a.length;
+    checkedElements=0;
+    for (var i = a.length - 1; i >= 0; i--) {
+        a[i].parentElement.remove();
+    }
+    var a = document.querySelectorAll('.uncheck');
+    for (var i = a.length - 1; i >= 0; i--) {
+        a[i].parentElement.classList.remove('hide');
+    }
+    noOfActive();
+    footerDisplay();
+}
+//to hide/unhide footer
+function footerDisplay(){
+    if(totalElements>0){
+        document.getElementById('bottom-bar').classList.remove('hide');
+    }
+    else{
+        document.getElementById('bottom-bar').classList.add('hide');
+    }
 }
